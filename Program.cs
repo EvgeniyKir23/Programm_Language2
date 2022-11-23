195c3946f1d1b4008de4178112e18e397aec410e
Евгений Кириллов <KirillovEE@pssochi.ru>
2022-11-23 09:58:20 +0300

    Save_Day of the week

--- Program.cs
+++ Program.cs
@@ -1,2 +1,39 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.WriteLine("Является ли указанный день выходным?");
+Console.Write("Введите номер дня недели ");
+int x = Convert.ToInt32(Console.ReadLine());
+if ( x == 1 )
+{
+    Console.WriteLine("понедельник - нет");
+}
+if ( x == 2 )
+{
+    Console.WriteLine("вторник - нет");
+}
+if ( x == 3 )
+{
+    Console.WriteLine("среда - нет");
+}
+if ( x == 4 )
+{
+    Console.WriteLine("четверг - нет");
+}
+if ( x == 5 )
+{
+    Console.WriteLine("пятница - нет");
+}
+if ( x == 6 )
+{
+    Console.WriteLine("суббота - да");
+}
+if ( x == 7 )
+{
+    Console.WriteLine("воскресенье - да");
+}
+if ( x > 7)
+{
+    Console.WriteLine("нет такого дня недели");
+}
+if ( x < 1 )
+{
+    Console.WriteLine("нет такого дня недели");
+}
